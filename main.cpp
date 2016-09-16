4fcb1a4b630a1a939fb35281f0914a94b33a0d65
Will Jackson <wrejcodes@gmail.com>
2016-09-15 19:29:44 -0500

    Added the best part

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ int main(){
 	std::cout << "What is love?"  << std::endl;
 	std::cout << "Baby don't hurt me" << std::endl;
 	std::cout << "NO MOAR!!" << std::endl;
+
+	for(int i = 0; i < 10; i++){
+		std::cout << "Woah woah Woaheeeoooh Woaheeeoooh Aheoeoooohh" << std::endl;
+		
+	}
 	return 0;
 }
