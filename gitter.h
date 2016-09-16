a79269347ee2817c7a1b2b5910780e85db7136e0
Dustin Freeman <dustin.rdf@gmail.com>
2016-09-15 19:47:26 -0500

    See if this shows an error for you

---
+++ gitter.h
@@ -0,0 +1,23 @@
+#ifndef GITTER_H
+#define GITTER_H
+
+
+#include <iostream>
+#include <string>
+
+class Gitter 
+{
+private:
+	std::string doYouEvenGit;
+
+public:
+	Gitter(std::string doYou)
+	{
+		doYouEvenGit = doYou;
+	}
+
+};
+
+
+
+#endif
