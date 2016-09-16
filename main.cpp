da09473fef533dad19fe76d2149f3c1cbb946c99
Dustin Freeman <dustin.rdf@gmail.com>
2016-09-15 19:29:07 -0500

    Find the error!

--- main.cpp
+++ main.cpp
@@ -4,5 +4,13 @@ int main(){
 	std::cout << "What is love?"  << std::endl;
 	std::cout << "Baby don't hurt me" << std::endl;
 	std::cout << "NO MOAR!!" << std::endl;
+
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		std::cout << "This thing never ends....why!?" << endl;
+	}
 	return 0;
 }
